ce69c4f1542ebb5bd5140a0681eff9af8eeffeb9
Elizabeth Brown <ej2brown@edu.uwaterloo.ca>
2020-03-27 02:20:22 +0000

    Implementedmedia query to change fixed width to fluid layout. Added toggle feature to let user hide the new tweet section

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,90 +6,108 @@
 
 
 $(() => {
-  const renderTweets = function (tweets) {
+
+  const loadTweets = () => {
+    $.ajax({
+      url: '/tweets/',
+      type: 'GET',
+      dataType: 'JSON'
+    })
+      .then(response => {
+        console.log('=====RESPONSE======',response);
+        formatRenderTweets(response);
+      })
+      .catch(() => {
+        const errorMessage =
+          `<div class="error">
+          <h1>Whoops, something went wrong!</h1>
+          </div> `;
+        $('#tweet-container').append(errorMessage);
+      })
+  
+  }
+
+  loadTweets();
+
+  const formatRenderTweets = function (tweets) {
     const markupArray = [];
     // loops through tweets
+    console.log('=====TWEETS=====', tweets)
     for (const tweet of tweets) {
+      
       // calls createTweetElement for each tweet
       markupArray.push(createTweetElement(tweet));
     }
     // takes return value and appends it to the tweets container
     $('#tweets-container').empty();
     $('#tweet-container').append(markupArray.reverse().join(''));
-
+    
   }
+  
+  
+  const createTweetElement = function (JSNObjTweet) {
+    const { name, avatar, handle } = JSNObjTweet.user;
+    const { text } = JSNObjTweet.content;
+    const { created_at } = JSNObjTweet;
 
-
-  const createTweetElement = function (tweet) {
     let renderedTweet = `
     <article class="tweet">
-      ${escape(JSON.stringify(tweet))} 
+    <h1><img src="${avatar}"> ${name} ${handle}</h1>
+    <p>${text}</p>
+    <p>${created_at}</p>
     </article>
-  `
+    `
+    // ${escape(JSON.stringify(tweet))} 
     return renderedTweet
 
   }
 
-  // renderTweets(data);
-
-  // $(document).ready(() => {
-  //   //send/POST request to the server 
-  //   $('.button').submit(function (event) {
-  //     event.preventDefault()
-  //     $.ajax('clients.js', { method: 'POST' })
-  //       .then(function (tweetList) {
-  //         console.log('Success: ', tweetList);
-  //         $button.replaceWith.serialize(tweetList);
-  //       })
-  //   })
-  // })
-
-  const loadTweets = () => {
-    $.ajax({
-      url: '/tweets/',
-      type: 'GET',
-      dataType: 'json',
-      success: response => {
-        renderTweets(response);
-      }
-    })
-
-  }
-
-  loadTweets();
 
   //POST the text data to '/tweets/ route
   $('.new-tweet-form').submit(function (event) {
     event.preventDefault();
-
+    $('.error').hide();
     if ($('#tweet-text').val().length !== 0 && $('#tweet-text').val().length <= 140) {
       $.ajax({
         url: '/tweets/',
         type: 'POST',
         data: $(this).serialize(),
-        success: () => {
-          console.log('worked');
-          loadTweets();
-        }
       })
+        .then(() => {
+          loadTweets();
+        })
+        .catch(() => {
+          const errorMessage =
+            `<div class="error">
+            <h1>Whoops, something went wrong!</h1>
+            </div> `;
+          $('#tweet-container').append(errorMessage);
+        })
     }
-    .catch(err) {
-      document.getElementById("demo").innerHTML = err.message;
-    }
-    
+
+
     if ($('#tweet-text').val().length === 0) {
-      alert('Error: No content');
+      $(".error").text("No content");
     }
+
     if ($('#tweet-text').val().length > 140) {
-      alert('Error: Too long (more than 140)');
+      $(".error").text('Error: Too long (more than 140)');
     }
     console.log('==========', event);
   });
 
-  const escape =  function(str) {
+  const escape = function (str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   }
 
+  $("#toggle").click(function () {
+    $(".new-tweet-container").slideToggle("slow", function () {
+    });
+  });
+
 });
+
+//toggle
+//$(selector).scrollTop(position)
