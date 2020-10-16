597c8346f65564d5db927f878ce91962c1b4d666
Elizabeth Brown <ej2brown@uwaterloo.ca>
2020-10-16 12:36:58 -0400

    add port env config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 'use strict';
 
 // Basic express setup:
-
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
