c4d0ce143875fa2476053763ad508ed95212d930
Elizabeth Brown <ej2brown@uwaterloo.ca>
2020-10-16 12:30:36 -0400

    rename to src directory

--- src/index.js
+++ src/index.js
@@ -9,10 +9,11 @@ const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
 
-app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(express.json());
+app.use(bodyParser.json());
 app.use(morgan('dev'));
+app.use(cors());
 
 // app.use(express.static('public'));
 
